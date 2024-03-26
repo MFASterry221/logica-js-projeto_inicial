869973f81cb8ef9972d5fdc57a12fc1ab9fc38f4
MFASterry221 <165020278+MFASterry221@users.noreply.github.com>
2024-03-26 11:29:33 -0300

    Update app.js

--- app.js
+++ app.js
@@ -2,9 +2,10 @@
 alert('Bem vindo ao jogo');
 
 //let é usado para declarar um variável. usa o sianl de "=" para atribuir um valor
-let numeroSecreto = Math.floor(Math.random() * 5);
+let numeroSecreto = Math.floor(Math.random() * 15);
+let numeroTentativas = 0;
 //parseInt para converter número em inteiro; prompt para caixa de entrada no navegador
-let numeroEscolhido = parseInt(prompt('Digite um número de 1 a 50'));
+let numeroEscolhido = parseInt(prompt('Digite um número de 0 a 15'));
 
 //if eu eu uso para testar uma lógica! "==" para igual, "> ou <"maior ou menor," >= ou <="
 //else é usado par ao se não, ou seja, quando um teste lógico do if der false.  
@@ -16,3 +17,16 @@ if(numeroSecreto == numeroEscolhido){
     }else {
         alert(`Você digitou ${numeroEscolhido} que é um número menor que o número secreto`);
     }
+
+while(numeroTentativas <= 2) {
+    let numeroEscolhido = parseInt(prompt('Digite um número de 0 a 50'));
+    if(numeroSecreto == numeroEscolhido){
+        alert(`Parabéns você acertou o número ${numeroSecreto} `);
+        break;
+        }else if(numeroEscolhido > numeroSecreto){
+            alert(`Você digitou ${numeroEscolhido} que é um número maior que o número secreto`);
+        }else {
+            alert(`Você digitou ${numeroEscolhido} que é um número menor que o número secreto`);
+        }
+    numeroTentativas = numeroTentativas + 1;
+}
