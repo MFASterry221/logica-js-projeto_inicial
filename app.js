1c37e8c39f4cdc3588f2f84899339c69b7f44b24
MFASterry221 <165020278+MFASterry221@users.noreply.github.com>
2024-03-27 09:19:40 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 alert('Bem vindo ao jogo do número secreto');
 
 let numeroSecreto = 3;
-let numeroDigitado = parseInt(prompt('Digite um número de 0 a 1'));
+let numeroDigitado = parseInt(prompt('Digite um número de 0 a 5'));
 
 if(numeroDigitado === numeroSecreto){
     alert('Você não errou, parabéns!');
