26c7f58cfd92dd4036b88d0a249f6668fd9954c2
MFASterry221 <165020278+MFASterry221@users.noreply.github.com>
2024-03-27 10:49:26 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
 alert('Bem vindos ao jogo do número secreto');
 
 let numeroSecreto = Math.floor(Math.random() * 5);
-alert(`o número secreto é ${numeroSecreto}`);
+let tentativas = 1;
 
-let numeroDigitado = parseInt(prompt('Digite um número de 0 a 5'));
-
-if(numeroDigitado === numeroSecreto){
-    alert('Você não errou, parabéns!');
+while(tentativas > 0){
+    let numeroDigitado = parseInt(prompt('Digite um número de 0 a 5'));
+    if(numeroDigitado === numeroSecreto){ 
+        alert('Você não errou, parabéns!');
+        break;
 }else if(numeroDigitado > numeroSecreto){
     alert('Você digitou o número que é maior que o número secreto');
 }else{
     alert('Você digitou o número que é menor que o número secreto');
 }
+alert(`você tem ${tentativas} tentativas ainda!`);
+tentativas = tentativas -1;
+}
+alert(`o número secreto é ${numeroSecreto}`);
