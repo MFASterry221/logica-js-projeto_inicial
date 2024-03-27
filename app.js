23dd53586b62ebca38d30986c7a0c74990e5b411
MFASterry221 <165020278+MFASterry221@users.noreply.github.com>
2024-03-27 10:16:49 -0300

    Update app.js

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ let numeroDigitado = parseInt(prompt('Digite um número de 0 a 5'));
 
 if(numeroDigitado === numeroSecreto){
     alert('Você não errou, parabéns!');
+}else if(numeroDigitado > numeroSecreto){
+    alert('Você digitou o número que é maior que o número secreto');
 }else{
-    alert('Você não acertou, tente novamente!');
+    alert('Você digitou o número que é menor que o número secreto');
 }
