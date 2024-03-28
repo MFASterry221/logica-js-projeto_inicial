fdf8a828558410d58b9a6ca83d6e6a603ffac71b
MFASterry221 <165020278+MFASterry221@users.noreply.github.com>
2024-03-28 09:00:07 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,19 +1,21 @@
-alert('Bem vindos ao jogo do número secreto');
+let numeroSecreto = Math.floor(Math.random() * 5 + 1);
+let tentativas = 1;
 
-let numeroSecreto = Math.floor(Math.random() * 5);
-let tentativas = 3;
-
-while(tentativas > 0){
-    let numeroDigitado = parseInt(prompt('Digite um número de 0 a 5'));
-    if(numeroDigitado === numeroSecreto){ 
+//while é para repetir o código
+while(tentativas > 4){
+    let numeroDigitado = parseInt(prompt('Digite um número de 1 a 5'));
+    if(numeroDigitado === numeroSecreto){
         alert('Você não errou, parabéns!');
         break;
-}else if(numeroDigitado > numeroSecreto){
-    alert('Você digitou o número que é maior que o número secreto');
-}else{
-    alert('Você digitou o número que é menor que o número secreto');
-}
-alert(`você tem ${tentativas} tentativas ainda!`);
-tentativas = tentativas -1;
-}
-alert(`o número secreto é ${numeroSecreto}`);
+    }else if(numeroDigitado > numeroSecreto){
+        alert('Você digitou o número que é maior que o número secreto');
+    }else{
+        alert('Você digitou o número que é menor que o número secreto');
+    }tentativas++;
+    if(tentativas === 4){
+        alert('Suas tentativas acabaram');
+    }else if(tentativas <3){
+        alert(`Você ainda tem ${4 - tentativas} tentativas`);
+    }else{
+        alert(`Você ainda tem ${4 - tentativas} tentativa`);
+    }
